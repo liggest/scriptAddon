49e1c82b283dbbf9f4e77e297043fab420f9b8c9
liggest <liggest@sina.cn>
2023-09-09 02:32:11 +0900

    Update defs from utility.lua
    Definition of GetID

--- library/definition.lua
+++ library/definition.lua
@@ -13,3 +13,17 @@ aux = Auxiliary
 ---@alias OperationFun fun(e,tp,eg,ep,ev,re,r,rp)
 
 ---@alias FilterFun fun(c:Card, ...)
+
+
+POS_FACEUP_DEFENCE = POS_FACEUP_DEFENSE
+POS_FACEDOWN_DEFENCE = POS_FACEDOWN_DEFENSE
+RACE_CYBERS = RACE_CYBERSE
+
+
+---@class CardTable : Card
+
+---获取 self_table, self_code, offset
+---@return CardTable self_table
+---@return integer self_code
+---@return integer offset  1 or 100
+function GetID() end
